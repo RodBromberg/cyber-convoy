7e916c5d479492912676001fafc2bbc47116cf9a
Rod Bromberg <brombergrod@gmail.com>
2023-08-23 16:58:46 -0400

    rendering 4,4,2 and added api keys to dotenv file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+// import './App.css';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_URL = `https://api.unsplash.com/photos/?client_id=${API_KEY}`;
+
+const imageContainerStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  boxSizing: 'border-box',
+};
+
+const imageStyle = {
+  width: '100%',
+  height: 250,
+  boxSizing: 'border-box',
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState([]);
+
+  async function fetchImages() {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setImages(data);
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={imageContainerStyle}>
+      {images.map((image, index) => (
+        <div key={index} style={{ width: '25%', boxSizing: 'border-box' }}>
+          <img style={imageStyle} src={image.urls.full} alt={`Image ${index}`} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
